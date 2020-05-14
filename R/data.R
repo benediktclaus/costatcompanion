db5338c10d05e19937fee2499f5a54c564f4d290
Benedikt <benediktclaus@msn.com>
2020-05-14 12:51:10 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,86 @@
+#' Intelligence ratings of Ravenclaw's
+#'
+#' A dataset containing intelligence ratings for 48 members of the Hogwarts
+#' house of Ravenclaw. This was used as an example for a one-sample t-Test.
+#'
+#' @format A data frame with 49 rows and 2 variables.
+#'
+#' \describe{
+#'     \item{id}{Participant's ID}
+#'     \item{intelligence}{Participant's
+#'   intelligence}
+#' }
+#'
+#' @source Fictional dataset.
+#'
+"ravenclaw"
+
+
+#' Chivalry ratings of Gryffindor's and Slytherin's
+#'
+#' A dataset containing chivalry ratings for 80 members of the Hogwarts houses
+#' Gryffindor and Slytherin. This was used as an example for an independent
+#' t-Test.
+#'
+#' @format A data frame with 80 rows and 3 variables.
+#'
+#' \describe{
+#'   \item{id}{Participant's ID}
+#'   \item{house}{Participant's house}
+#'   \item{chivalry}{Participant's chivalry rating}
+#' }
+#'
+#' @source Fictional dataset.
+"chivalry"
+
+
+#' Patience ratings of Hufflepuffs
+#'
+#' A dataset containing patience ratings for 106 members of the Hogwarts
+#' houseHufflepuff. This was used as an example for a dependent t-Test.
+#'
+#' @format A data frame with 106 rows and 3 variables.
+#'
+#' \describe{
+#'   \item{id}{Participant's ID}
+#'   \item{measurement}{Time of measurement in relation to the school year}
+#'   \item{patience}{Participant's patience}
+#' }
+#'
+#' @source Fictional dataset.
+"patience"
+
+
+#' Joy of Phantasialand's visitors
+#'
+#' A dataset containing joy ratings for 186 visitors of the Phantasialand. This
+#' was used as an example dataset for a one-way ANOVA.
+#'
+#' @format A data frame with 186 rows and 3 variables.
+#'
+#' \describe{
+#'   \item{id}{Participant's ID}
+#'   \item{backpack}{Participant's backpack}
+#'   \item{joy}{Participant's joy}
+#' }
+#'
+#' @source Fictional dataset.
+"phantasialand"
+
+
+#' Yumminessof different ice cream flavors
+#'
+#' A dataset containing yumminess ratings of 295 voluntary tasters for different
+#' ice cream flavors. This was used as an example dataset for a factorial ANOVA.
+#'
+#' @format A data frame with 295 rows and 4 variables.
+#'
+#' \describe{
+#'   \item{id}{Participant's ID}
+#'   \item{flavor}{Ice cream flavor}
+#'   \item{sex}{Participant's sex}
+#'   \item{yumminess}{Ice cream's yumminess}
+#' }
+#'
+#' @source Fictional dataset.
+"ice_cream"
---
+++ data-raw/datasets.R
@@ -0,0 +1,74 @@
+## code to prepare `DATASET` dataset goes here
+
+# Pakete ------------------------------------------------------------------
+library(tidyverse)
+
+
+# t-Tests -----------------------------------------------------------------
+# Einstichproben-t-Test
+set.seed(20200422)
+ravenclaw <- tibble("intelligence" = round(rnorm(48, 125, 15))) %>%
+  rownames_to_column(var = "id")
+
+use_data(ravenclaw, overwrite = TRUE)
+
+
+# t-Test für unabhängige Stichproben
+set.seed(20200422)
+chivalry <- tibble(
+  house = as_factor(c("Gryffindor", "Slytherin")),
+  chivalry = list(round(rnorm(n = 42, mean = 95, sd = 8)), round(rnorm(n = 38, mean = 45, sd = 9)))
+) %>%
+  unnest(chivalry) %>%
+  rownames_to_column(var = "id")
+
+use_data(chivalry, overwrite = TRUE)
+
+
+# t-Test für verbundene Stichproben
+set.seed(20200422)
+patience <- tibble(
+  measurement = as_factor(c("Begin", "End")),
+  patience = list(round(rnorm(53, 44, 8)), round(rnorm(53, 46, 9)))
+) %>%
+  unnest(patience) %>%
+  add_column(id = as.character(rep(1:53, 2)), .before = "measurement")
+
+use_data(patience, overwrite = TRUE)
+
+
+# ANOVAs ------------------------------------------------------------------
+# Einfaktorielle ANOVA
+set.seed(20200428)
+phantasialand <- tibble(
+  backpack = as_factor(c("None", "Light", "Heavy", "Handcart")),
+  joy = list(round(rnorm(42, 9, 1.3)), round(rnorm(53, 7, 1.5)), round(rnorm(56, 6.5, 1.5)), round(rnorm(35, 3, 1.7)))
+) %>%
+  unnest(joy) %>%
+  mutate(joy = pmin(joy, 10),
+         joy = pmax(joy, 0)) %>%
+  sample_n(size = nrow(.)) %>%
+  rownames_to_column(var = "id")
+
+use_data(phantasialand, overwrite = TRUE)
+
+# Faktorielle ANOVA
+set.seed(20200504)
+ice_cream <- crossing(flavor = as_factor(c("Vanilla", "Chocolate", "Cookie", "Champagne")),
+         sex = as_factor(c("Female", "Male"))) %>%
+  add_column(yumminess = list(
+    round(rnorm(36, 17, 2)),
+    round(rnorm(38, 16, 2.1)),
+    round(rnorm(41, 17.5, 1.9)),
+    round(rnorm(40, 17.3, 1.7)),
+    round(rnorm(45, 16, 2.3)),
+    round(rnorm(35, 13, 2.4)),
+    round(rnorm(40, 15.5, 2.2)),
+    round(rnorm(20, 8, 2.4))
+  )) %>%
+  unnest(yumminess) %>%
+  mutate(yumminess = pmin(yumminess, 20)) %>%
+  sample_n(size = nrow(.)) %>%
+  rownames_to_column(var = "id")
+
+use_data(ice_cream, overwrite = TRUE)
